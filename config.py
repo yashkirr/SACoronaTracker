aa079a995151e2e7680a8267a5596cad4e7d7429
Yashkir Ramsamy - RMSYAS003 <rmsyas003@myuct.ac.za>
2020-07-13 21:48:57 +0200

    make it better

---
+++ config.py
@@ -0,0 +1,13 @@
+"""
+Author: Yashkir Ramsamy
+Contact: me@yashkir.co.za
+Date: 2020/07/13
+
+Purpose: API Keys for SACoronaTracker
+"""
+
+api_key = "YVraN1CxvIl5eL5S56mnUlIav"
+api_key_secret = "FZpnxqcQhHSa3FoNdJsH7vuqJGcB0PUYb3HJ1KE0Az5IjBjXjQ"
+access_token = "1282418028567822337-8zrEIwYh3akAKwPHYqB1rh6R7aMKUY"
+access_token_secret = "1TVuMDAprWgHHBhvDx6gzbA37V8QzUPIOxgIujz5ZLJ5x"
+yashkirr_id =1066958004636065792
